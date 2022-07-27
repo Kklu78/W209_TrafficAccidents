fe9ce34a9e4ac7af8e8773d58ae64b6f97f4acf8
Kklu78 <76934184+Kklu78@users.noreply.github.com>
2022-07-26 19:13:14 -0700

    added text

--- flaskapp.py
+++ flaskapp.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = -1
+
 
 @app.route("/")
 def flaskapp():
-    file="about9.jpg"
-    return render_template("flaskapp.html",file=file)
+    file = "about9.jpg"
+    return render_template("flaskapp.html", file=file)
+
 
 if __name__ == "__main__":
     app.run()
